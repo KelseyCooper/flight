d52386d8c0bab1cbc6e945b6511576aac150a086
Kelsey Cooper <kcoop@live.ca>
2018-02-21 10:41:57 -0800

    added a responsive container to the color chart making it mobile friendly

--- client/src/components/ColorChart.jsx
+++ client/src/components/ColorChart.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { PieChart, Pie, Cell, LabelList } from "recharts";
-
+import { PieChart, Pie, Cell, LabelList, ResponsiveContainer } from "recharts";
 
 class ColroChartComponent extends Component {
   constructor(props) {
@@ -17,27 +16,31 @@ class ColroChartComponent extends Component {
 
   componentWillReceiveProps(nextProps) {
     this.setState({
-      data:this.props.data,
-      colors: ["#f44162", "#65686d", "#6c8ce2", "#3a3e47"]})
+      data: this.props.data,
+      colors: ["#f44162", "#65686d", "#6c8ce2", "#3a3e47"]
+    });
   }
 
   render() {
     return <div>
-        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
-          <Pie data={this.state.data} dataKey="value" cx={120} cy={200} innerRadius={60} outerRadius={80} fill="#8884d8" paddingAngle={5}>
-            {this.state.data.map((entry, index) => (
-              <Cell
-                key={index}
-                fill={this.state.colors[index % this.state.colors.length]}
-              />
-            ))}
-            <LabelList stroke="none" offset={15} dataKey="value" position="outside" />
-          </Pie>
-        </PieChart>
+        <ResponsiveContainer width="100%" height={400}>
+          <PieChart onMouseEnter={this.onPieEnter}>
+            <Pie data={this.state.data} dataKey="value" innerRadius={60} outerRadius={80} fill="#8884d8" paddingAngle={5}>
+              {this.state.data.map((entry, index) => (
+                <Cell
+                  key={index}
+                  fill={this.state.colors[index % this.state.colors.length]}
+                />
+              ))}
+              <LabelList stroke="none" offset={15} dataKey="value" position="outside" />
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
         {this.state.data.map((item, index) => {
-          return (
-            <div key={index}> {item.name} {item.value}</div>
-          )
+          return <div key={index}>
+              {" "}
+              {item.name} {item.value}
+            </div>;
         })}
       </div>;
   }
