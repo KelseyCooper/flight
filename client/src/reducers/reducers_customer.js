798cf931bec6c9a97d5f7f429e5e9344d2b2b7a4
Kelsey Cooper <kcoop@live.ca>
2018-02-24 09:34:43 -0800

    made customer state immutable

--- client/src/reducers/reducers_customer.js
+++ client/src/reducers/reducers_customer.js
@@ -1,11 +1,28 @@
-import {
-  CUSTOMER_FETCHED
-} from "../actions/actions_customers";
+import { CUSTOMER_FETCHED } from "../actions/actions_customers";
 
-const customer = (state = [], action) => {
+const initialState = {
+  id: 0,
+  name: null,
+  email: null,
+  gender: null,
+  age: null,
+  reason_to_buy: null,
+  purchased: []
+};
+
+const customer = (state = initialState, action) => {
   switch (action.type) {
     case CUSTOMER_FETCHED:
-      return action.payload;
+      return {
+        ...state,
+        id: action.payload.id,
+        name: action.payload.name,
+        email: action.payload.email,
+        gender: action.payload.gender,
+        age: action.payload.age,
+        reason_to_buy: action.payload.reason_to_buy,
+        purchased: action.payload.purchased
+      };
     default:
       return state;
   }
