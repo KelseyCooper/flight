50e0f6940a7afaa41baeb40407c3c32fa61abc1c
Kelsey Cooper <kcoop@live.ca>
2018-02-26 10:59:31 -0800

    removed constructor from Main.jsx fully integrating Redux

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,18 +3,10 @@ import Nav from './Nav';
 import Main from './Main';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      someInfo: ''
-    }
-  }
-  
 
   render() {
     return <div>
         <Nav />
-        <div className="container">{this.state.someInfo}</div>
         <Main />
       </div>;
   }
